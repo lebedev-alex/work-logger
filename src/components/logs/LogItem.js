2ff20607064e31a9b8c871ca8a5bcc7ce1f77fd7
A. Lebedev <lebedev96@gmail.com>
2020-05-12 15:15:17 +0300

    classNames updated

--- src/components/logs/LogItem.js
+++ src/components/logs/LogItem.js
@@ -8,20 +8,20 @@ const LogItem = ({ log }) => (
       <a
         href="#edit-log-modal"
         className={`modal-trigger ${
-          log.attention === 'true' ? 'red-text' : 'blue-text'
+          log.attention === 'true' ? 'pink-text text-lighten-3' : ''
         }`}
       >
         {log.message}
       </a>
       <br />
       <span className="grey-text">
-        <span className="white-text">id #{log.id}</span>
+        <span className="light-blue-text text-lighten-5">id #{log.id}</span>
         {''} last updated by {''}
-        <span className="white-text">{log.worker}</span>
+        <span className="light-blue-text text-lighten-5">{log.worker}</span>
         {''} on <Moment format="MMMM do YYYY, h:mm:ss a">{log.date}</Moment>
       </span>
       <a href="#!" className="secondary-content">
-        <i className="material-icons white-text">delete</i>
+        <i className="material-icons light-blue-text text-lighten-5">delete</i>
       </a>
     </div>
   </li>
